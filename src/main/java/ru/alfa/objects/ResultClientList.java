9da40c68b36b9d5e5ed6ae838b30dcda29304864
Dmitry Mikhajlyuk <mikhajlyuk@gmail.com>
2017-07-04 08:51:46 +0300

    get ClientPhone

---
+++ src/main/java/ru/alfa/objects/ResultClientList.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.alfa.objects;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Namespace;
+import org.simpleframework.xml.NamespaceList;
+import org.simpleframework.xml.Root;
+
+@Root(name = "list")
+@Namespace(reference = "http://WSCommonTypes10.CS.ws.alfabank.ru", prefix = "p548")
+public class ResultClientList {
+
+    @Attribute (name = "xsi:type")   
+    private String type = "p548:WSTypeAddInfoAlphanumeric";
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    } 
+
+    @Element
+    private String anm;
+    @Element
+    private String adt;
+
+    public String getAnm() {
+        return anm;
+    }
+
+    public void setAnm(String anm) {
+        this.anm = anm;
+    }
+
+    public String getAdt() {
+        return adt;
+    }
+
+    public void setAdt(String adt) {
+        this.adt = adt;
+    }
+}
---
+++ src/main/java/ru/alfa/requests/RequestRetrofit.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.alfa.requests;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.convert.AnnotationStrategy;
+import org.simpleframework.xml.core.Persister;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+import ru.alfa.objects.ResultClientPhone;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class RequestRetrofit {
+
+    public ResultClientPhone postWSCustomerExtendedInfoCL(String xml) throws IOException, Exception {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://vuwsint:9081/")
+                .client(new OkHttpClient())
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .build();
+
+        PostRequest service = retrofit.create(PostRequest.class);
+
+        RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/xml; charset=utf-8"), xml);
+
+        Call<ResponseBody> call = service.postClietnPhone(body);
+        Response<ResponseBody> responseBody = call.execute();
+        String bodyXML = responseBody.body().string();
+        if (bodyXML != null) {
+            Serializer serializer = new Persister();
+            ResultClientPhone resultClientPhone = serializer.read(ResultClientPhone.class, bodyXML);
+            return resultClientPhone;
+        }
+        return null;
+    }
+
+}
